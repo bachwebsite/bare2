72a3beef54a21ea96a11125e86b2996cbf42662c
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-15 20:08:16 -0500

    n

--- public/data/js/color.js
+++ public/data/js/color.js
@@ -1,10 +1,18 @@
-function loadColorFromLocalStorage(varName, colorPickerId, coloredElementId) {
+function loadColorFromLocalStorage(varName, colorPickerClass, coloredElementClass) {
     console.log('Loading color from localStorage');
     const savedColor = localStorage.getItem(varName);
     if (savedColor) {
         document.documentElement.style.setProperty(varName, savedColor);
-        document.getElementById(colorPickerId).value = savedColor;
-        document.getElementById(coloredElementId).style.backgroundColor = savedColor;
+        const colorPickers = document.querySelectorAll(colorPickerClass);
+        colorPickers.forEach(picker => {
+            if (picker.value === savedColor) {
+                picker.checked = true;
+            }
+        });
+        const coloredElements = document.querySelectorAll(coloredElementClass);
+        coloredElements.forEach(element => {
+            element.style.backgroundColor = savedColor;
+        });
     }
     console.log('Saved color:', savedColor);
 }
@@ -12,13 +20,16 @@ function loadColorFromLocalStorage(varName, colorPickerId, coloredElementId) {
 function changeColor(varName, event) {
     const selectedColor = event.target.value;
     document.documentElement.style.setProperty(varName, selectedColor);
-    coloredElementId = varName === '--c1' ? 'coloredElement1' : 'coloredElement2';
-    document.getElementById(coloredElementId).style.backgroundColor = selectedColor;
-    
+    const coloredElementClass = varName === '--c1' ? '.coloredElement1' : '.coloredElement2';
+    const coloredElements = document.querySelectorAll(coloredElementClass);
+    coloredElements.forEach(element => {
+        element.style.backgroundColor = selectedColor;
+    });
+
     localStorage.setItem(varName, selectedColor);
 }
 
 window.addEventListener('load', function() {
-    loadColorFromLocalStorage('--c1', 'colorPicker1', 'coloredElement1');
-    loadColorFromLocalStorage('--c2', 'colorPicker2', 'coloredElement2');
+    loadColorFromLocalStorage('--c1', '.color-picker', '.coloredElement1');
+    loadColorFromLocalStorage('--c2', '.color-picker', '.coloredElement2');
 });
