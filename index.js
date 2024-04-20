dccb6c2bd99b4d2d203735295cdc009ee3722823
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-19 23:15:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ server.on("upgrade", (req, socket, head) => {
     socket.end();
   }
 });
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 2100;
 server.on("listening", () => {
   console.log('UP')
 });
