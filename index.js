f60eb9c73725aa4eea5bfdf10ba56d2aa60399f4
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-19 23:00:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,115 +1,37 @@
-import express from 'express'
-import basicAuth from 'express-basic-auth'
-import http from 'node:http'
-import { createBareServer } from '@tomphttp/bare-server-node'
-import path from 'node:path'
-import cors from 'cors'
-import c from './c.js'
+const http = require("node:http");
+const { createBareServer } = require("@tomphttp/bare-server-node");
+const fs = require("fs");
+const path = require("path");
+const url = require('url');
+const express = require('express');
+const app = express();
 
-const __dirname = process.cwd()
-const server = http.createServer()
-const app = express(server)
-const bareServer = createBareServer('/o/')
-const PORT = process.env.PORT || 8080
+app.use(express.static('public'));
 
-if (c.challenge) {
-  console.log('Password protection is enabled. Usernames are: ' + Object.keys(c.users))
-  console.log('Passwords are: ' + Object.values(c.users))
+// Create an HTTP server
+const httpServer = http.createServer();
+const bareServer = createBareServer("/bare/");
 
-  app.use(
-    basicAuth({
-      users: c.users,
-      challenge: true,
-    })
-  )
-}
+httpServer.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }});
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
-
-if (c.routes !== false) {
-  const routes = [
-    { path: '/chemistry', file: 'a.html' },
-    { path: '/trig', file: 'g.html' },
-    { path: '/quadratics', file: 's.html' },
-    { path: '/', file: 'index.html' },
-    { path: '/devices', file: 'p.html' },
-  ]
-
-  routes.forEach((route) => {
-    app.get(route.path, (req, res) => {
-      res.sendFile(path.join(__dirname, 'static', route.file))
-    })
-  })
-}
-
-if (c.local !== false) {
-  app.get('/e/*', (req, res, next) => {
-    const baseUrls = [
-      'https://raw.githubusercontent.com/v-5x/x/fixy',
-      'https://raw.githubusercontent.com/ypxa/y/main',
-      'https://raw.githubusercontent.com/ypxa/w/master',
-    ]
-    fetchData(req, res, next, baseUrls)
-  })
-}
-
-const fetchData = async (req, res, next, baseUrls) => {
-  try {
-    const reqTarget = baseUrls.map((baseUrl) => `${baseUrl}/${req.params[0]}`)
-    let data
-    let asset
-
-    for (const target of reqTarget) {
-      asset = await fetch(target)
-      if (asset.ok) {
-        data = await asset.arrayBuffer()
-        break
-      }
-    }
-
-    if (data) {
-      res.end(Buffer.from(data))
-    } else {
-      res.status(404).send()
-    }
-  } catch (error) {
-    console.error(`Error fetching ${req.url}:`, error)
-    res.status(500).send()
+  httpServer.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
   }
-}
+})
 
-app.get('*', (req, res) => {
-  res.status(404).send();
+httpServer.on("listening", () => {
+  console.log("HTTP server listening");
+  console.log(`View your server at http://localhost:${port}`);
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send();
+httpServer.listen({
+  port: 2100,
 });
-
-server.on('request', (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res)
-  } else {
-    app(req, res)
-  }
-})
-
-server.on('upgrade', (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head)
-  } else {
-    socket.end()
-  }
-})
-
-server.on('listening', () => {
-  console.log(`Running at http://localhost:${PORT}`)
-})
-
-server.listen({
-  port: PORT,
-})
