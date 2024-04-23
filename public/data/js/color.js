d6a5cd190cd8ede7a82f538a5816de290b5a8184
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-22 19:44:24 -0500

    Update color.js

--- public/data/js/color.js
+++ public/data/js/color.js
@@ -1,43 +1,47 @@
-function loadColorFromLocalStorage(varName, colorPickerClass, coloredElementClass) {
-    console.log('Loading color from localStorage');
-    const savedColor = localStorage.getItem(varName);
-    if (savedColor) {
-        document.documentElement.style.setProperty(varName, savedColor);
-        const colorPickers = document.querySelectorAll(colorPickerClass);
-        colorPickers.forEach(picker => {
-            if (picker.value === savedColor) {
-                picker.checked = true;
-            }
-        });
-        const coloredElements = document.querySelectorAll(coloredElementClass);
-        coloredElements.forEach(element => {
-            element.style.backgroundColor = savedColor;
-        });
-    }
-    console.log('Saved color:', savedColor);
+function loadColorFromLocalStorage(varName, colorPickerClass) {
+  console.log("Loading color from localStorage");
+  const savedColor = localStorage.getItem(varName);
+  if (savedColor) {
+    document.documentElement.style.setProperty(varName, savedColor);
+    const colorPickers = document.querySelectorAll(colorPickerClass);
+    colorPickers.forEach((picker) => {
+      if (picker.value === savedColor) {
+        picker.checked = true;
+      }
+    });
+    const coloredElements = document.querySelectorAll("[data-color]");
+    coloredElements.forEach((element) => {
+      if (element.dataset.color === varName) {
+        element.style.backgroundColor = savedColor;
+      }
+    });
+  }
+  console.log("Saved color:", savedColor);
 }
 
 function changeColor(varName, event) {
-    const selectedColor = event.target.value;
-    document.documentElement.style.setProperty(varName, selectedColor);
-    const coloredElementClass = varName === '--c1' ? '.coloredElement1' : '.coloredElement2';
-    const coloredElements = document.querySelectorAll(coloredElementClass);
-    coloredElements.forEach(element => {
-        element.style.backgroundColor = selectedColor;
-    });
+  const selectedColor = event.target.value;
+  document.documentElement.style.setProperty(varName, selectedColor);
 
-    localStorage.setItem(varName, selectedColor);
+  const coloredElements = document.querySelectorAll("[data-color]");
+  coloredElements.forEach((element) => {
+    if (element.dataset.color === varName) {
+      element.style.backgroundColor = selectedColor;
+    }
+  });
+
+  localStorage.setItem(varName, selectedColor);
 }
 
-window.addEventListener('load', function() {
-    loadColorFromLocalStorage('--c1', '#colorPicker1', '.coloredElement1');
-    loadColorFromLocalStorage('--c2', '#colorPicker2', '.coloredElement2');
-    loadColorFromLocalStorage('--c3', '#colorPicker3', '.coloredElement3');
+window.addEventListener("load", function () {
+  loadColorFromLocalStorage("--c1", "#colorPicker1");
+  loadColorFromLocalStorage("--c2", "#colorPicker2");
+  loadColorFromLocalStorage("--c3", "#colorPicker3");
 
-    const colorPickers = document.querySelectorAll('.color-picker');
-    colorPickers.forEach(picker => {
-        picker.addEventListener('input', function(event) {
-            changeColor(this.dataset.varName, event);
-        });
+  const colorPickers = document.querySelectorAll(".color-picker");
+  colorPickers.forEach((picker) => {
+    picker.addEventListener("input", function (event) {
+      changeColor(this.dataset.varName, event);
     });
+  });
 });
