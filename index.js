38ede90a3ef1357673e0f51fe35b43ac871057cd
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-25 18:16:38 -0500

    fgh

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-import { createBareServer } from '@tomphttp/bare-server-node';
+import { createBareServer } from "@tomphttp/bare-server-node";
 import express from "express";
 import { createServer } from "node:http";
-import { SocksProxyAgent } from 'socks-proxy-agent';
-const socksProxyAgent = new SocksProxyAgent('socks://localhost:40000');
-import { uv } from "@titaniumnetwork-dev/ultraviolet";
+import { SocksProxyAgent } from "socks-proxy-agent";
+const socksProxyAgent = new SocksProxyAgent("socks://localhost:40000");
+import pkg from "@titaniumnetwork-dev/ultraviolet";
+const uv = pkg.uv;
 import { join } from "node:path";
-import { hostname } from "node:os";
-import dotenv from 'dotenv';
+import { hostname } from "os";
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
-const publicPath = fileURLToPath(new URL("./public/", import.meta.url));
-const bare = createBareServer('/bare/', {});
+import serveStatic from "serve-static";
+
+const __dirname = fileURLToPath(import.meta.url);
+const publicPath = join(__dirname, "../public");
+
+const bare = createBareServer("/bare/", {});
 const app = express();
 dotenv.config();
-app.use(express.static(publicPath));
-app.use("/b/uv/", express.static(uv));
-app.use("/uv/", express.static(uv));
+app.use(serveStatic('public'));
+app.use(express.static("uv"));
+
 const server = createServer();
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) {
@@ -23,6 +28,7 @@ server.on("request", (req, res) => {
     app(req, res);
   }
 });
+
 server.on("upgrade", (req, socket, head) => {
   if (bare.shouldRoute(req)) {
     bare.routeUpgrade(req, socket, head);
@@ -30,11 +36,12 @@ server.on("upgrade", (req, socket, head) => {
     socket.end();
   }
 });
+
 const port = process.env.PORT || 2100;
 server.on("listening", () => {
-  console.log('UP')
+  console.log("Server is up and running!");
 });
 
-server.listen({
-  port,
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
