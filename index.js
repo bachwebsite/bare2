60325a94a204353c98bbfca013b6fca728da85b1
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-25 20:16:01 -0500

    dfg

--- index.js
+++ index.js
@@ -1,69 +1,80 @@
-import express from "express";
-import http from "node:http";
-import path from "node:path";
-import createBareServer from "@tomphttp/bare-server-node";
-import request from "@cypress/request";
+import express from 'express';
+import http from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import cors from 'cors';
+import path from "path";
+import { hostname } from "node:os"
 
-
-const __dirname = path.resolve();
 const server = http.createServer();
 const app = express(server);
-const bareServer = createBareServer("/bare/");
-
+const __dirname = process.cwd();
+const bareServer = createBareServer('/bare/');
+const PORT = 2100;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+app.use(express.static(__dirname + '/public'));
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/index.html'));
 });
-app.get("/g", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/g.html"));
+app.get('/g', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/g.html'));
 });
-app.get("/a", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/a.html"));
+
+app.get('/s', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/s.html'));
 });
-app.get("/p", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/p.html"));
+
+app.get('/a', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/a.html'));
 });
-app.get("/t", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/t.html"));
+
+app.get('/p', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/p.html'));
 });
-app.get("./public/data/js/worker.js", (req, res) => {
-  request("./public/data/js/worker.js", (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      res.setHeader("Content-Type", "text/javascript");
-      res.send(body);
+
+app.get('/t', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/t.html'));
+});
+
+server.on('request', (req, res) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res)
     } else {
-      res.status(500).send("Error fetching worker script");
+        app(req, res)
     }
-  });
-});
-app.use((req, res) => {
-  res.statusCode = 404;
-  res.sendFile(path.join(__dirname, "./public/404.html"));
-});
+})
 
+server.on('upgrade', (req, socket, head) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeUpgrade(req, socket, head)
+    } else {
+        socket.end()
+    }
+})
 
-server.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-server.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-server.on("listening", () => {
-  console.log(
-    `breakium running on port https://localhost:${server.address().port}`
-  );
-});
+server.on('listening', () => {
+    const address = server.address();
 
+    console.log(`\thttp://localhost:${address.port}`);
+    console.log(`\thttp://${hostname()}:${address.port}`);
+    console.log(
+        `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
+        }:${address.port}`
+    );
+})
 
-server.listen({ port: 2100 });
+server.listen({ port: PORT, })
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+    console.log("SIGTERM signal received: closing HTTP server");
+    server.close();
+    bareServer.close();
+    process.exit(0);
+}
--- public/uv/sw.js
+++ public/uv/sw.js
@@ -1,1 +1,7 @@
-importScripts('/uv/uv.sw.js'); const sw = new UVServiceWorker(); self.addEventListener('fetch', event => event.respondWith(sw.fetch(event)));
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts(__uv$config.sw || '/uv/uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
