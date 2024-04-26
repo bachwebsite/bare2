335c23898e2063dbaade35e39f90846d6a85f2e1
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-26 08:07:30 -0500

    Update index.js

--- public/data/js/index.js
+++ public/data/js/index.js
@@ -1,10 +1,8 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 
-
 form.addEventListener('submit', async event => {
     event.preventDefault();
-    
     window.navigator.serviceWorker.register('./sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
