7bef18b2332c418a03cc63ccb1ac309a8cbe3339
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-27 15:24:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,77 @@
-import http from "node:http";
 import { createBareServer } from "@tomphttp/bare-server-node";
-import fs from "fs";
-import path, { join } from "path";
-import url from 'url';
-import express from 'express';
+// const createBareServer = bareServerNode.createBareServer;
+import express from "express";
+import http from "http";
+import { dirname, join } from "path";
+import { hostname } from "node:os";
 import { fileURLToPath } from "url";
+import compression from 'compression';
+import chalk from 'chalk';
+import 'dotenv/config'
 
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+const bare = createBareServer("/bare/");
 const app = express();
 
-app.use(express.static('public'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const publicPath = fileURLToPath(new URL("public", import.meta.url));
-
-// Create an HTTP server
-const httpServer = http.createServer();
-const bareServer = createBareServer("/bare/");
+app.use(compression());
+app.use(express.static(__dirname + "/static/"));
+app.use("/class/", express.static(__dirname + "/services/uv/"));
+app.use("/work/", express.static(__dirname + "/services/dynamic/"));
 
 app.use((req, res) => {
   res.status(404);
-  res.sendFile(join(publicPath, "404.html"));
+  res.send(
+    `<style>body{font-family:Roboto,sans-serif;background-color:#111;text-align:center;padding:50px;}.container{background-color:#111;border-radius:5px;box-shadow:0px 0px 10px rgba(0,0,0,0.1);padding:20px;}h1{font-size:48px;color:#fff;margin-bottom:20px;}p{font-size:18px;color:#fff;}a{text-decoration:none;color:lightgreen;}a:hover{text-decoration:underline;}</style><div class="container"><h1>404 :(</h1><p>The page you are looking for might have been removed or does not exist. If you opened Abyss inside of Abyss, this page will also show up.</p><p>Open a new tab to continue.</a></p></div>`
+  );
 });
 
-httpServer.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
   } else {
     app(req, res);
   }
 });
 
-httpServer.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
   } else {
     socket.end();
   }
 });
 
-httpServer.on("listening", () => {
-  console.log("HTTP server listening");
-  console.log("View your server at http://localhost:2100");
+server.on("listening", () => {
+  const address = server.address();
+  console.log("Breakium listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
 });
 
-httpServer.listen({
-  port: 2100,
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  bare.close();
+  process.exit(0);
+}
+
+server.listen({
+  port,
 });
