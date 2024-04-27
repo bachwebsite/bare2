65986edef7873a85705e07ac57e2e4dfdf3ba2d0
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-27 15:34:52 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,26 @@
-importScripts('/uv/uv.sw.js');
+importScripts("/dynamic/dynamic.config.js");
+importScripts("/dynamic/dynamic.worker.js");
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
+importScripts("/uv/uv.sw.js");
 
-const sw = new UVServiceWorker();
+const uv = new UVServiceWorker();
+const dynamic = new Dynamic();
 
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
-);
+self.dynamic = dynamic;
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async function () {
+      if (await dynamic.route(event)) {
+        return await dynamic.fetch(event);
+      }
+
+      if (event.request.url.startsWith(location.origin + "/classes/math/")) {
+        return await uv.fetch(event);
+      }
+
+      return await fetch(event.request);
+    })()
+  );
+});
